be36c0bbdf455ea2effe4bd08452fef28947807e
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-03-30 10:33:16 -0500

    Fixed wrong log statement

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const possibleMatch = process.env.BRANCH.split("_")[0].match(/((?!([A-Z0-9a-z]{1,10})-?$)[A-Z]{1}[A-Z0-9]+-\d+)/g);
 
 if (!possibleMatch) {
+    console.error("Branch name should be formatted like the following PV-345_this_is_a_branch.  Please check your branch name and try again.")
     process.exit(1)
 } else {
-    console.log("Branch name should be formatted like the following PV-345_this_is_a_branch.  Please check your branch name and try again.")
     process.exit(0)
 }
