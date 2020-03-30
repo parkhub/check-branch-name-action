2bc4f729b54088ef461889d6de53aaaa93b54758
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-03-30 10:30:06 -0500

    Added code for aciton

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-console.log('##########################################')
-console.log('##########################################')
-console.log('##########################################')
-console.log('##########################################')
-console.log(process.env.BRANCH)
-console.log('##########################################')
-console.log('##########################################')
-console.log('##########################################')
-console.log('##########################################')
+const possibleMatch = process.ENV.BRANCH.split("_")[0].match(/((?!([A-Z0-9a-z]{1,10})-?$)[A-Z]{1}[A-Z0-9]+-\d+)/g);
+
+if (!possibleMatch) {
+    process.exit(1)
+} else {
+    console.log("Branch name should be formatted like the following PV-345_this_is_a_branch.  Please check your branch name and try again.")
+    process.exit(0)
+}
